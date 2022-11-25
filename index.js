6f65bb4c5ff084a271fe6b9e4f5f86c579c0c5c3
Kevin-aps <kevin.thompson@thinkaps.com>
2022-11-25 12:54:15 +0000

    add calendar options drop down

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 const register = document.getElementById("register");
-const calendar = document.getElementById("calendar");
+const calendarBtn = document.getElementById("calendar-button");
 const buttons = document.querySelectorAll('[class~="btn"]');
 const welcome = document.getElementById("welcome-container");
 const facultyMembers = document.querySelector("#faculty");
 
-
-  buttons.forEach((button) => {
-    if (button.innerText === "READ MORE" || button.innerHTML.includes("READ MORE")) {
-      button.setAttribute("data-bs-toggle", "modal");
-      button.setAttribute("data-bs-target", "#facultyModal");
-    }
-  });
-
+buttons.forEach((button) => {
+  if (
+    button.innerText === "READ MORE" ||
+    button.innerHTML.includes("READ MORE")
+  ) {
+    button.setAttribute("data-bs-toggle", "modal");
+    button.setAttribute("data-bs-target", "#facultyModal");
+  }
+});
 
 const addActiveClass = (e) => {
-  console.log(e);
   let element = document.getElementById(e.target.id);
   if (!element.classList.contains("active")) {
     element.classList.add("active");
+    addShowClass(e);
   } else {
     element.classList.remove("active");
+    removeShowClass(e);
   }
 };
 
+const addShowClass = (e) => {
+  const parent = e.target.parentElement;
+  parent.lastElementChild.classList.add("show");
+};
+
+const removeShowClass = (e) => {
+  const parent = e.target.parentElement;
+  parent.lastElementChild.classList.remove("show");
+};
+
 register.addEventListener("click", addActiveClass);
-calendar.addEventListener("click", addActiveClass);
-
-
+calendarBtn.addEventListener("click", addActiveClass);
